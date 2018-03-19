8044ebcf34c605c58e7eda39fc0e067f490fb5ad
Michael Hellwig <michaelhellwig@gmail.com>
2018-03-19 11:34:04 +0100

    Update plot_ExpDynamics.m

--- plot_ExpDynamics.m
+++ plot_ExpDynamics.m
@@ -4,7 +4,6 @@ p1=figure;
 axes('Parent',p1,'Position',[0.15 0.13 0.75 0.70],'LineWidth',2,'FontSize',16);
 set(gcf,'color','w');
 set(gcf,'position',[1 600 800 600]);
-       
 loglog(dyn.fev,dyn.f,'b','LineWidth',1);
     hold on;
     ylabel('population size \lambda');
@@ -18,8 +17,7 @@ loglog(dyn.fev,dyn.f,'b','LineWidth',1);
 p2=figure;
 axes('Parent',p2,'Position',[0.15 0.13 0.75 0.70],'LineWidth',2,'FontSize',16);
 set(gcf,'color','w');
-set(gcf,'position',[1 600 800 600]);
-       
+set(gcf,'position',[1 600 800 600]);   
 loglog(dyn.fev,dyn.sigma,'b','LineWidth',1);
     hold on;
     ylabel('mutation strength \sigma');
@@ -33,13 +31,26 @@ loglog(dyn.fev,dyn.sigma,'b','LineWidth',1);
 p3=figure;
 axes('Parent',p3,'Position',[0.15 0.13 0.75 0.70],'LineWidth',2,'FontSize',16);
 set(gcf,'color','w');
-set(gcf,'position',[1 600 800 600]);
-       
+set(gcf,'position',[1 600 800 600]);  
 loglog(dyn.fev,dyn.lambda,'b','LineWidth',1);
     hold on;
     ylabel('population size \lambda');
     xlabel('function evaluations');
     xlim([min(dyn.fev) max(dyn.fev)])
     ylim([0.9*min(dyn.lambda) 1.1*max(dyn.lambda)])
+    grid on;
+    title([input.fname ' b_i=i^' num2str(input.exp) ', N=' num2str(input.n) ', L=' num2str(input.L) ', \sigma_\epsilon=' num2str(input.noise_strength)])
+    
+    
+p3=figure;
+axes('Parent',p3,'Position',[0.15 0.13 0.75 0.70],'LineWidth',2,'FontSize',16);
+set(gcf,'color','w');
+set(gcf,'position',[1 600 800 600]);  
+loglog(dyn.fev,dyn.condC,'b','LineWidth',1);
+    hold on;
+    ylabel('condition number of C');
+    xlabel('function evaluations');
+    xlim([min(dyn.fev) max(dyn.fev)])
+    ylim([0.9*min(dyn.condC) 1.1*max(dyn.condC)])
     grid on;
     title([input.fname ' b_i=i^' num2str(input.exp) ', N=' num2str(input.n) ', L=' num2str(input.L) ', \sigma_\epsilon=' num2str(input.noise_strength)])
